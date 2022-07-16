8c10bb27d87d664bc0f2532486fdb356337bb477
JohnII7 <157798378@qq.com>
2022-07-16 11:44:18 +0800

    万能类型与类型断言

---
+++ struct/interface2.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// interface{}是万能数据类型, 与interface不同, 多了一个花括号
+func myFunc(arg interface{}) {
+	fmt.Println("myFunc is called")
+	fmt.Println(arg)
+
+	// 给interface{}提供断言机制
+	value, ok := arg.(string)
+	if !ok {
+		fmt.Println("arg is not string type")
+	} else {
+		fmt.Println("arg is string type, value:", value)
+		fmt.Printf("value type:%T", value)
+	}
+}
+
+type Book struct {
+	auth string
+}
+
+func main() {
+	book := Book{"Golang"}
+	myFunc(book)
+	myFunc("Hello")
+}
