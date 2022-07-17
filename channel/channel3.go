8e244bbcab6c42e533c162a65fbb96066c0253d7
JohnII7 <157798378@qq.com>
2022-07-17 10:52:34 +0800

    channel关闭的特点, 读取的range写法

--- channel/channel3.go
+++ channel/channel3.go
@@ -12,13 +12,19 @@ func main() {
 		// 关闭一个channel
 		close(c)
 	}()
-	for {
-		// 如果ok为true表示channel没有关闭，false表示channel已经关闭
-		if data, ok := <-c; ok {
-			fmt.Println(data)
-		} else {
-			break
+	/*
+		for {
+			// 如果ok为true表示channel没有关闭，false表示channel已经关闭
+			if data, ok := <-c; ok {
+				fmt.Println(data)
+			} else {
+				break
+			}
 		}
+	*/
+	// 也可以使用range来迭代操作channel
+	for data := range c {
+		fmt.Println(data)
 	}
 	fmt.Println("main finished")
 }
